a2ed5e85e2d5ab12fff642b45ef411fea9fb5a92
Rieben Axel <axel.rieben@he-arc.ch>
2018-01-08 16:19:58 +0100

    Reindent and start assignation

--- parser5.py
+++ parser5.py
@@ -1,84 +1,109 @@
 import ply.yacc as yacc
 
-from lex5 import tokens
 import AST
+
+precedence = (
+    ('left', 'ADD_OP'),
+    ('left', 'MUL_OP'),
+    ('right', 'UMINUS'),
+)
+
+operations = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
 
 vars = {}
 
-def p_programme_statement(p):
-    ''' programme : statement '''
-    p[0] = AST.ProgramNode(p[1])
+
+def p_programme(p):
+    ''' programme : statement | statement programme'''
+    try:
+        p[0] = AST.ProgramNode([p[1]] + p[2].children)
+    except Exception as e:
+        p[0] = AST.ProgramNode(p[1])
+
 
 def p_programme_recursive(p):
     ''' programme : statement ';' programme '''
-    p[0] = AST.ProgramNode([p[1]]+p[3].children)
+    p[0] = AST.ProgramNode([p[1]] + p[3].children)
+
 
 def p_statement(p):
     ''' statement : assignation
         | structure '''
     p[0] = p[1]
-    	
+
+
 def p_statement_print(p):
     ''' statement : PRINT expression '''
     p[0] = AST.PrintNode(p[2])
 
+
 def p_structure(p):
     ''' structure : WHILE expression '{' programme '}' '''
-    p[0] = AST.WhileNode([p[2],p[4]])
+    p[0] = AST.WhileNode([p[2], p[4]])
+
 
 def p_expression_op(p):
     '''expression : expression ADD_OP expression
             | expression MUL_OP expression'''
     p[0] = AST.OpNode(p[2], [p[1], p[3]])
-    	
+
+
 def p_expression_num_or_var(p):
     '''expression : NUMBER
         | IDENTIFIER '''
     p[0] = AST.TokenNode(p[1])
-    	
+
+
 def p_expression_paren(p):
     '''expression : '(' expression ')' '''
     p[0] = p[2]
-    	
+
+
 def p_minus(p):
     ''' expression : ADD_OP expression %prec UMINUS'''
     p[0] = AST.OpNode(p[1], [p[2]])
-    	
+
+
 def p_assign(p):
     ''' assignation : IDENTIFIER '=' expression '''
-    p[0] = AST.AssignNode([AST.TokenNode(p[1]),p[3]])
+    p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+
+
+def p_assign_bool(p):
+    ''' assignation : BOOL IDENTIFIER '=' TRUE '\n'
+    | BOOL IDENTIFIER '=' FALSE '\n' '''
+    vars[p[2]] = ('BOOL', p[4])
+    p[0] = AST.AssignDeclareNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(p[4])])
+
 
 def p_error(p):
-    if p:
-        print ("Syntax error in line %d" % p.lineno)
-        yacc.errok()
-    else:
-        print ("Sytax error: unexpected end of file!")
+    print("Syntax error in line %d" % p.lineno)
 
-
-precedence = (
-    ('left', 'ADD_OP'),
-    ('left', 'MUL_OP'),
-    ('right', 'UMINUS'),  
-)
 
 def parse(program):
     return yacc.parse(program)
 
+
 yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
-    import sys 
-    	
+    import sys
+
     prog = open(sys.argv[1]).read()
     result = yacc.parse(prog)
     if result:
-        print (result)
-            
+        print(result)
+
         import os
+
         graph = result.makegraphicaltree()
-        name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
-        graph.write_pdf(name) 
-        print ("wrote ast to", name)
+        name = os.path.splitext(sys.argv[1])[0] + '-ast.pdf'
+        graph.write_pdf(name)
+        print("wrote ast to", name)
     else:
-        print ("Parsing returned no result!")
+        print("Parsing returned no result!")
